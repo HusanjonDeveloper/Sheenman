b7988e1ad40fe51536d03ce7c9ee1223318fd20b
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-06 17:59:04 +0500

    ShouldAddGuestAsync -> Fail

---
+++ Sheenman.Api/Servies/Foundetions/Guests/IGuestServies.cs
@@ -0,0 +1,15 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free To Use To find Comfort and Peace 
+//===================================================
+
+using System.Threading.Tasks;
+using Sheenman.Api.Models.Foundetions.Guests;
+
+namespace Sheenman.Api.Servies.Foundetions.Guests
+{
+    public interface IGuestServies
+    {
+        ValueTask<Guest> AddGuestAsync(Guest guest);
+    }
+}
---
+++ Sheenmanapi.Test.Unit/Servies/Foundetions/Guests/GuestServiesTests.Logic.Add.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenman.Api.Models.Foundetions.Guests;
+using Xunit;
+
+namespace Sheenmanapi.Test.Unit.Servies.Foundetions.Guests
+{
+    public partial class GuestServiesTests
+    {
+        [Fact]
+
+        public async Task ShouldAddGuestAsync()
+        {
+            // Given
+          
+            Guest randomGuest = CreateRandomGuest();
+            Guest inputGuest = randomGuest;
+            Guest persistedGuest = inputGuest;
+            Guest excepdetGuest = persistedGuest.DeepClone();
+
+            this.StoregeBrokerMock.Setup(broker =>
+            broker.InsertGuestAsync(inputGuest)).ReturnsAsync(persistedGuest);
+            
+            // When
+            
+            Guest actualGuest = await this.guestServies
+                .AddGuestAsync(inputGuest);
+
+            // Then 
+
+            actualGuest.Should().BeEquivalentTo(persistedGuest);
+
+            this.StoregeBrokerMock.Verify(broker =>
+            broker.InsertGuestAsync(inputGuest), Times.Once());
+
+            this.StoregeBrokerMock.VerifyNoOtherCalls();
+        }
+
+      
+    }
+}
