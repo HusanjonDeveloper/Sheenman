ca908e1b1b2bd5edb65f29ad96b5daecb63ba238
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-06 18:50:58 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ Sheenmanapi.Test.Unit/Servies/Foundetions/Guests/GuestServiesTests.Validation.Add.cs
@@ -0,0 +1,36 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free ToUse Comfort and Peace 
+//===================================================
+
+using Sheenman.Api.Models.Foundetions.Guests;
+using Sheenman.Api.Servies.Foundetions.Guests.Exceptions;
+using Xunit;
+
+namespace Sheenmanapi.Test.Unit.Servies.Foundetions.Guests
+{
+    public partial class GuestServiesTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync()
+        {
+            // Given 
+
+            Guest nullGuest = null;
+            var nullGuestException = new NullGuestException();
+
+            var expectedGuestValidationException = 
+                new GuestValidationException(nullGuestException);
+
+            // When
+
+            ValueTask<Guest> addGuestTask =
+                this.guestServies.AddGuestAsync(nullGuest);
+
+            // Then
+
+            await Assert.ThrowsAsync<GuestValidationException>(() =>
+            addGuestTask.AsTask());
+        }
+    }
+}
