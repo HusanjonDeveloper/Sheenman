5f357389a755dc05cbb2fef06a2d7b65c32cf1c2
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-10 06:05:13 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync->FAIL

--- Sheenmanapi.Test.Unit/Servies/Foundetions/Guests/GuestServiesTests.cs
+++ Sheenmanapi.Test.Unit/Servies/Foundetions/Guests/GuestServiesTests.cs
@@ -3,6 +3,7 @@
 // Free To Use To find Comfort and Peace 
 //===================================================
 
+using System.Linq.Expressions;
 using FluentAssertions;
 using Moq;
 using Sheenman.Api.Brokers.Loggings;
@@ -10,6 +11,7 @@ using Sheenman.Api.Brokers.Storeges;
 using Sheenman.Api.Models.Foundetions.Guests;
 using Sheenman.Api.Servies.Foundetions.Guests;
 using Tynamix.ObjectFiller;
+using Xeptions;
 using Xunit;
 
 namespace Sheenmanapi.Test.Unit.Servies.Foundetions.Guests
@@ -30,11 +32,18 @@ namespace Sheenmanapi.Test.Unit.Servies.Foundetions.Guests
                  loggingBroker: this.loggingBrokerMock.Object);
         }
 
-        private DateTimeOffset GetRandomDateTimeOffset() =>
-            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
-
         private Guest CreateRandomGuest() =>
             CreateGuestFiller(dates:GetRandomDateTimeOffset()).Create();
+        private DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+        
+        private Expression<Func<Xeption, bool>> SomeExceptionAs(Xeption expectedException)
+        {
+            return actualException =>
+            actualException.Message == expectedException.Message
+            &&  actualException.InnerException.Message == expectedException.InnerException.Message
+            && (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
+        }
 
         private Filler<Guest> CreateGuestFiller(DateTimeOffset dates)
         {
