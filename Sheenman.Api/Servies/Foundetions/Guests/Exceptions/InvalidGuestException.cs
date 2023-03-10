661c52d7767d83bc8f2c042c3f8eea86f27dba40
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2023-03-10 19:38:33 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsInvalidAndLogItAsync->FAIL

---
+++ Sheenman.Api/Servies/Foundetions/Guests/Exceptions/InvalidGuestException.cs
@@ -0,0 +1,16 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers 
+// Free ToUse Comfort and Peace 
+//===================================================
+
+using Xeptions;
+
+namespace Sheenman.Api.Servies.Foundetions.Guests.Exceptions
+{
+    public class InvalidGuestException:Xeption
+    {
+        public InvalidGuestException()
+          :base(message:"Guest is invalid")
+        {}
+    }
+}
--- Sheenmanapi.Test.Unit/Servies/Foundetions/Guests/GuestServiesTests.Validation.Add.cs
+++ Sheenmanapi.Test.Unit/Servies/Foundetions/Guests/GuestServiesTests.Validation.Add.cs
@@ -43,7 +43,74 @@ namespace Sheenmanapi.Test.Unit.Servies.Foundetions.Guests
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.StoregeBrokerMock.VerifyNoOtherCalls();
-           
+
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+
+        public async Task ShouldThrowValidationExceptionOnAddIfGuestIsInvalidAndLogItAsync(
+         string invalindText)
+        {
+            // given 
+            var invalindGuest = new Guest
+            {
+                FristName = invalindText
+            };
+
+            var invalidGuestException = new InvalidGuestException();
+
+            invalidGuestException.AddData(
+                key: nameof(Guest.id),
+                values: "Id is requred");
+
+            invalidGuestException.AddData(
+                key: nameof(Guest.FristName),
+                values: "Text is requred");
+
+            invalidGuestException.AddData(
+                key: nameof(Guest.LastName),
+                values: "Text is requred");
+            invalidGuestException.AddData(
+                key: nameof(Guest.DateOgBrith),
+                values: "Date");
+
+                invalidGuestException.AddData(
+            key: nameof(Guest.Adders),
+            values: "Text is required");
+
+            invalidGuestException.AddData(
+                key: nameof(Guest.Email),
+                values: "Text is required");
+
+            var exceptedGuestValidationException =
+                new GuestValidationException(invalidGuestException);
+
+            // when 
+            ValueTask<Guest> addGuestTask =
+                this.guestServies.AddGuestAsync(invalindGuest);
+
+            // then 
+
+            await Assert.ThrowsAsync<GuestValidationException>(() =>
+            addGuestTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SomeExceptionAs(
+                exceptedGuestValidationException))),
+                Times.Once());
+
+            this.StoregeBrokerMock.Verify(broker =>
+            broker.InsertGuestAsync(It.IsAny<Guest>()),
+            Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.StoregeBrokerMock.VerifyNoOtherCalls();
+            
+                
+        }
+        
     }
 }
